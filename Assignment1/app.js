6fcc6110429ae445c9b1bbb6912a5ab6a29b06b7
Dhrushit Raval <dr9703@rit.edu>
2021-09-22 10:30:41 -0400

    adding keyframe data

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -20,12 +20,11 @@ const positionAttribute = geometry.getAttribute("position");
 
 const colors = [];
 const color = new THREE.Color();
-console.log(positionAttribute)
+console.log(positionAttribute);
 
 for (let i = 0; i < positionAttribute.count; i += 6) {
-	
 	color.set(Math.random() * 0xffffff);
-	
+
 	// define the same color for each vertex of a triangle
 	colors.push(color.r, color.g, color.b);
 	colors.push(color.r, color.g, color.b);
@@ -36,7 +35,7 @@ for (let i = 0; i < positionAttribute.count; i += 6) {
 	colors.push(color.r, color.g, color.b);
 }
 
-geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
@@ -50,27 +49,38 @@ cube.position.y = -50;
 var direction = 1;
 
 //
+// fetch the key frame file
+//
+// let keyframeString = await loadTextResource("./keyframe-input.txt");
+
+let keyFrameString =
+	"0.0  0.0 0.0 0.0 1.0 1.0 -1.0 0.0 \n \
+1.0  4.0 0.0 0.0 1.0 1.0 -1.0 30.0 \n \
+2.0  8.0 0.0 0.0 1.0 1.0 -1.0 90.0 \n \
+3.0  12.0 12.0 12.0 1.0 1.0 -1.0 180.0 \n \
+4.0  12.0 18.0 18.0 1.0 1.0 -1.0 270.0 \n \
+5.0  18.0 18.0 18.0 0.0 1.0 0.0 90.0 \n \
+6.0  18.0 18.0 18.0 0.0 0.0 1.0 90.0 \n \
+7.0  25.0 12.0 12.0 1.0 0.0 0.0 0.0 \n \
+8.0  25.0 0.0 18.0 1.0 0.0 0.0 0.0 \n \
+9.0  25.0 1.0 18.0 1.0 0.0 0.0 0.0";
+
+
+let keyFrames = parseKFString(keyFrameString);
+let kfAnim = new KFAnimator(keyFrameString);
+
+console.log(keyFrameString);
+
+//
 // render
 //
+
+cube.position.x = 
+
 function animate() {
 	requestAnimationFrame(animate);
 
-	var elapsedTime = clock.getElapsedTime() % 40;
-	if (elapsedTime > 20) {
-		elapsedTime = 40 - elapsedTime;
-	}
-
-	// console.log(elapsedTime);
-	const posX = -50 + 5 * elapsedTime;
-	const posY = -50 + 5 * elapsedTime;
-	// console.log(elapsedTime);
-	const rotY_degrees = 18 * elapsedTime;
-	const rotY_radians = (rotY_degrees * 2 * Math.PI) / 360;
-
-	cube.position.x = posX;
-	cube.position.y = posY;
-
-	cube.rotation.y = rotY_radians;
+	
 	renderer.render(scene, camera);
 }
 animate();
