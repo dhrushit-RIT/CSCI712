df568d0afa0c36b4c58fa5a59483cb4f1d77d2d4
Dhrushit Raval <dr9703@rit.edu>
2021-09-22 10:31:33 -0400

    classes needed for the implementation

--- Assignment1/utilclasses.js
+++ Assignment1/utilclasses.js
@@ -1,12 +1,28 @@
 class Keyframe {
 	time;
 	pos;
-	rot;
+	orientation;
 
 	constructor(time, x, y, z, xa, ya, za, theeta) {
 		this.time = time;
-		this.pos = Position(x, y, z);
-		this.rot = Rotation(xa, ya, za, theeta);
+		this.pos = new Position(x, y, z);
+		this.orientation = new Orientation(xa, ya, za, theeta);
+	}
+
+	static createFromString(kfString) {
+		let WHITE_SPACE_RE = /[ ,]+/;
+		let [t, x, y, z, xa, ya, za, theeta] = kfString.split(WHITE_SPACE_RE);
+
+		return new Keyframe(
+			parseFloat(t),
+			parseFloat(x),
+			parseFloat(y),
+			parseFloat(z),
+			parseFloat(xa),
+			parseFloat(ya),
+			parseFloat(za),
+			parseFloat(theeta)
+		);
 	}
 }
 
@@ -18,11 +34,36 @@ class Position {
 	}
 }
 
-class Rotation {
+class Orientation {
 	constructor(xa, ya, za, theeta) {
 		this.xa = xa;
 		this.ya = ya;
 		this.za = za;
 		this.theeta = theeta;
+	}
+}
+
+class KFAnimator {
+	previousKF = null;
+	nextKF = null;
+	keyframes = {};
+	constructor(kfstring) {
+		this.parseKFString(kfstring);
+		this.previousKF = this.keyframes[0];
+		this.nextKF = this.keyframes[1];
+	}
+
+	parseKFString(kfstring) {
+		let kfArray = {};
+
+		for (let kfstr of kfstring.split("\n")) {
+			let WHITE_SPACE_RE = /[ ,]+/;
+			let kf = Keyframe.createFromString(kfstr);
+			this.keyframes[t] = kf;
+		}
+	}
+
+	getKFAt(time) {
+		return this.keyframes[index];
 	}
 }
