2fb020585a65888ec240f3502926cceb5fb25e93
Dhrushit Raval <dr9703@rit.edu>
2021-09-28 19:30:29 -0400

    adding separate class file for each of the classes

---
+++ Assignment1/src/classes/Position.ts
@@ -0,0 +1,14 @@
+class Position {
+	x: number;
+	y: number;
+	z: number;
+	constructor(x: number, y: number, z: number) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	toString(): string {
+		return "( " + this.x + " ," + this.y + " ," + this.z + " )";
+	}
+}
