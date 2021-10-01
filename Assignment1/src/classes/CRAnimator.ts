94909289ede544cb097c00087597641a8b3f5c00
Dhrushit Raval <dr9703@rit.edu>
2021-10-01 16:28:53 -0400

    implemented catmull rom interpolation

--- Assignment1/src/classes/CRAnimator.ts
+++ Assignment1/src/classes/CRAnimator.ts
@@ -1,4 +1,3 @@
-
 // DAT.GUI
 
 class CRAnimator {
@@ -9,9 +8,10 @@ class CRAnimator {
 	private keyframes: MyKeyframe[] = [];
 	private endTime: number;
 	private u: number = 0;
-	private controlSpeed = 0.05;
-	private simulate: boolean = true;
+	private controlSpeed = 0.02;
+	private simulate: boolean = false;
 	private totalKFs = -1;
+	private basisMatrix = new THREE.Matrix4();
 
 	// Catmul Rom stuff
 	private startFrame: MyKeyframe;
@@ -35,6 +35,14 @@ class CRAnimator {
 
 		this.startFrame = this.computeStartFrame();
 		this.endFrame = this.computeEndFrame();
+
+		// prettier-ignore
+		this.basisMatrix.set(
+			-0.5, 1.5, -1.5,  0.5, 
+			1,   -2.5,    2, -0.5, 
+			-0.5,   0,  0.5,    0, 
+			0,      1,    0,    0
+		);
 	}
 
 	private computeStartFrame() {
@@ -121,9 +129,57 @@ class CRAnimator {
 	}
 
 	interpolateCatmulRom(): Position {
-		if (this.currentKFIndex == 0) {
+		let p1: Position, p2: Position, p3: Position, p4: Position;
+
+		if (this.keyframes.length < 3) {
+			// p1 = this.keyframes[0].pos;
+			p1 = this.startFrame.pos;
+			p2 = this.keyframes[0].pos;
+			p3 = this.keyframes[1].pos;
+			// p4 = this.keyframes[1].pos;
+			p4 = this.endFrame.pos;
+		} else if (this.currentKFIndex == 0) {
+			p1 = this.startFrame.pos;
+			p2 = this.keyframes[0].pos;
+			p3 = this.keyframes[1].pos;
+			p4 = this.keyframes[2].pos;
+		} else if (this.nextKFIndex == this.keyframes.length - 1) {
+			p1 = this.keyframes[this.totalKFs - 3].pos;
+			p2 = this.keyframes[this.totalKFs - 2].pos;
+			p3 = this.keyframes[this.totalKFs - 1].pos;
+			p4 = this.endFrame.pos;
+		} else {
+			p1 = this.keyframes[this.currentKFIndex - 1].pos;
+			p2 = this.keyframes[this.currentKFIndex].pos;
+			p3 = this.keyframes[this.nextKFIndex].pos;
+			p4 = this.keyframes[this.nextKFIndex + 1].pos;
 		}
-		return new Position(0, 0, 0);
+
+		let uW = 1;
+		let uZ = uW * this.u;
+		let uY = uZ * this.u;
+		let uX = uY * this.u;
+		let U: THREE.Vector4 = new THREE.Vector4(uX, uY, uZ, uW);
+
+		let controlMatrix: THREE.Matrix4 = new THREE.Matrix4();
+		// prettier-ignore
+		controlMatrix.set(
+			p1.x, p1.y, p1.z, 0,
+			p2.x, p2.y, p2.z, 0,
+			p3.x, p3.y, p3.z, 0,
+			p4.x, p4.y, p4.z, 0
+		);
+
+		const catmulProduct = new THREE.Matrix4();
+		catmulProduct.multiplyMatrices(this.basisMatrix, controlMatrix);
+		console.log(catmulProduct);
+		catmulProduct.transpose()
+		U.applyMatrix4(catmulProduct);
+		return new Position(
+			U.getComponent(0),
+			U.getComponent(1),
+			U.getComponent(2)
+		);
 	}
 
 	getKFAt(time: number): MyKeyframe {
@@ -131,21 +187,20 @@ class CRAnimator {
 
 		this.updateFrames(time);
 		this.computeControlVariable(time);
+		console.log(this.u);
 
 		//
 		// interpolate position
 		//
 		let initialPosition: Position = this.currentKF.pos;
 		let finalPosition: Position = this.nextKF.pos;
-		let currentPosition: Position = new Position(
-			this.interpolateLinear(initialPosition.x, finalPosition.x, this.u),
-			this.interpolateLinear(initialPosition.y, finalPosition.y, this.u),
-			this.interpolateLinear(initialPosition.z, finalPosition.z, this.u)
-		);
+		let currentPosition: Position = this.interpolateCatmulRom();
+		// currentPosition.set();
 		// console.log(this.u);
 		//
 		// interpolate orientation
 		//
+		// console.log(currentPosition);
 		let qInitial = new THREE.Quaternion().copy(this.currentKF.quat);
 		let qFinal = this.nextKF.quat;
 		// qInitial.slerp(qFinal, u);
