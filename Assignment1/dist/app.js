8f6900979192398172a5a499baa9d4b5eb67bcca
Dhrushit Raval <dr9703@rit.edu>
2021-10-07 13:26:53 -0400

    udpating dist

--- Assignment1/dist/app.js
+++ Assignment1/dist/app.js
@@ -1,20 +1,26 @@
 const scene_linear = new THREE.Scene();
 const scene_catmull = new THREE.Scene();
 const scene_ut = new THREE.Scene();
+const scene_bzd = new THREE.Scene();
+debugger;
 const canvas_linear = document.getElementById("c1");
 const canvas_catmull = document.getElementById("c2");
 const canvas_ut = document.getElementById("c3");
+const canvas_bzd = document.getElementById("c4");
 const side = window.innerWidth / 3;
 const renderer_linear = new THREE.WebGLRenderer({ canvas: canvas_linear });
 const renderer_catmull = new THREE.WebGLRenderer({ canvas: canvas_catmull });
 const renderer_ut = new THREE.WebGLRenderer({ canvas: canvas_ut });
+const renderer_bzd = new THREE.WebGLRenderer({ canvas: canvas_bzd });
 renderer_linear.setSize(side, side);
 renderer_catmull.setSize(side, side);
 renderer_ut.setSize(side, side);
+renderer_bzd.setSize(side, side);
 let fieldOfView = 45, aspectRatio = 4 / 3, near = 0.1, far = 1000;
 const camera_linear = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
 const camera_catmull = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
 const camera_ut = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
+const camera_bzd = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
     vertexColors: false,
@@ -30,18 +36,23 @@ const materials = [
 const cube_linear = new THREE.Mesh(geometry, materials);
 const cube_catmull = new THREE.Mesh(geometry, materials);
 const cube_ut = new THREE.Mesh(geometry, materials);
+const cube_bzd = new THREE.Mesh(geometry, materials);
 camera_linear.position.set(0, 0, -20);
 camera_catmull.position.set(0, 0, -20);
 camera_ut.position.set(0, 0, -20);
+camera_bzd.position.set(0, 0, -20);
 camera_linear.lookAt(5, 0, 0);
 camera_catmull.lookAt(5, 0, 0);
 camera_ut.lookAt(5, 0, 0);
+camera_bzd.lookAt(5, 0, 0);
 scene_linear.add(cube_linear);
 scene_catmull.add(cube_catmull);
 scene_ut.add(cube_ut);
+scene_bzd.add(cube_bzd);
 scene_linear.add(camera_linear);
 scene_catmull.add(camera_catmull);
 scene_ut.add(camera_ut);
+scene_bzd.add(camera_bzd);
 const clock = new THREE.Clock();
 cube_linear.position.x = 0;
 cube_linear.position.y = 0;
@@ -52,6 +63,9 @@ cube_catmull.position.z = 0;
 cube_ut.position.x = 0;
 cube_ut.position.y = 0;
 cube_ut.position.z = 0;
+cube_bzd.position.x = 0;
+cube_bzd.position.y = 0;
+cube_bzd.position.z = 0;
 let keyFrameString = [
     "0.0  0.0 0.0 0.0 1.0 1.0 -1.0 0.0",
     "1.0  4.0 0.0 0.0 1.0 1.0 -1.0 30.0",
@@ -68,13 +82,16 @@ let keyFrames = parseKFString(keyFrameString);
 let kfAnim_linear = new KFAnimator(keyFrameString);
 let kfAnim_catmull = new CRAnimator(keyFrameString);
 let kfAnim_ut = new CRAnimator(keyFrameString);
+let kfAnim_bzd = new BezierAnimator(keyFrameString);
 kfAnim_ut.setMappedControl(true);
 cube_linear.position.set(keyFrames[0].pos.x, keyFrames[0].pos.y, keyFrames[0].pos.z);
 cube_catmull.position.set(keyFrames[0].pos.x, keyFrames[0].pos.y, keyFrames[0].pos.z);
 cube_ut.position.set(keyFrames[0].pos.x, keyFrames[0].pos.y, keyFrames[0].pos.z);
+cube_bzd.position.set(keyFrames[0].pos.x, keyFrames[0].pos.y, keyFrames[0].pos.z);
 cube_linear.quaternion.setFromAxisAngle(new THREE.Vector3(keyFrames[0].orientation.xa, keyFrames[0].orientation.ya, keyFrames[0].orientation.za), keyFrames[0].orientation.theeta);
 cube_catmull.quaternion.setFromAxisAngle(new THREE.Vector3(keyFrames[0].orientation.xa, keyFrames[0].orientation.ya, keyFrames[0].orientation.za), keyFrames[0].orientation.theeta);
 cube_ut.quaternion.setFromAxisAngle(new THREE.Vector3(keyFrames[0].orientation.xa, keyFrames[0].orientation.ya, keyFrames[0].orientation.za), keyFrames[0].orientation.theeta);
+cube_bzd.quaternion.setFromAxisAngle(new THREE.Vector3(keyFrames[0].orientation.xa, keyFrames[0].orientation.ya, keyFrames[0].orientation.za), keyFrames[0].orientation.theeta);
 console.log(keyFrameString);
 const canvas = renderer_linear.domElement;
 console.log(renderer_linear.domElement.clientHeight, renderer_linear.domElement.clientWidth);
@@ -89,11 +106,13 @@ function animate() {
         kfAnim_linear.resetFrames();
         kfAnim_catmull.resetFrames();
         kfAnim_ut.resetFrames();
+        kfAnim_bzd.resetFrames();
     }
     elapsedTime = elapsedTime % keyFrames[keyFrames.length - 1].time;
     let currentKF_linear = kfAnim_linear.getKFAt(elapsedTime);
     let currentKF_catmull = kfAnim_catmull.getKFAt(elapsedTime);
     let currentKF_ut = kfAnim_ut.getKFAt(elapsedTime);
+    let currentKF_bzd = kfAnim_bzd.getKFAt(elapsedTime);
     cube_linear.position.x = currentKF_linear.pos.x;
     cube_linear.position.y = currentKF_linear.pos.y;
     cube_linear.position.z = currentKF_linear.pos.z;
@@ -103,12 +122,17 @@ function animate() {
     cube_ut.position.x = currentKF_ut.pos.x;
     cube_ut.position.y = currentKF_ut.pos.y;
     cube_ut.position.z = currentKF_ut.pos.z;
+    cube_bzd.position.x = currentKF_bzd.pos.x;
+    cube_bzd.position.y = currentKF_bzd.pos.y;
+    cube_bzd.position.z = currentKF_bzd.pos.z;
     cube_linear.rotation.setFromQuaternion(currentKF_linear.quat);
     cube_catmull.rotation.setFromQuaternion(currentKF_catmull.quat);
     cube_ut.rotation.setFromQuaternion(currentKF_ut.quat);
+    cube_bzd.rotation.setFromQuaternion(currentKF_bzd.quat);
     renderer_linear.render(scene_linear, camera_linear);
     renderer_catmull.render(scene_catmull, camera_catmull);
     renderer_ut.render(scene_ut, camera_ut);
+    renderer_bzd.render(scene_bzd, camera_bzd);
 }
 animate();
 //# sourceMappingURL=app.js.map
--- Assignment1/dist/classes/Animators/BezierAnimator.js
+++ Assignment1/dist/classes/Animators/BezierAnimator.js
@@ -67,8 +67,17 @@ class BezierAnimator extends KFAnimator {
         console.log(this.u, controlMatrix);
         return new Position(U.getComponent(0), U.getComponent(1), U.getComponent(2));
     }
+    interpolateDeCasteljau() {
+        let q0 = super.interpolatePosition(this.controlPointsBefore[this.currentKFIndex], this.controlPointsAfter[this.currentKFIndex], this.u);
+        let q1 = super.interpolatePosition(this.controlPointsAfter[this.currentKFIndex], this.controlPointsBefore[this.nextKFIndex], this.u);
+        let q2 = super.interpolatePosition(this.controlPointsBefore[this.nextKFIndex], this.controlPointsAfter[this.nextKFIndex], this.u);
+        let r0 = super.interpolatePosition(q0, q1, this.u);
+        let r1 = super.interpolatePosition(q1, q2, this.u);
+        let p = super.interpolatePosition(r0, r1, this.u);
+        return p;
+    }
     interpolatePosition() {
-        return this.interpolateBezier();
+        return this.interpolateDeCasteljau();
     }
 }
 //# sourceMappingURL=BezierAnimator.js.map
