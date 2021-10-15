9065d0516c67d4bc3cbeb1066b996ad641b213cd
Dhrushit Raval <dr9703@rit.edu>
2021-10-15 16:51:16 -0400

    adding table and ball

--- Assignment2/src/app.ts
+++ Assignment2/src/app.ts
@@ -22,7 +22,11 @@ document.body.appendChild(renderer.domElement);
 //
 // add cube to the scene
 //
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const TABLE_LENGTH_FT = 12;
+const TABLE_WIDTH_FT = 6;
+const BALL_DIM_FT = 0.0859375;
+
+const geometry = new THREE.BoxGeometry(TABLE_LENGTH_FT, 0.1, TABLE_WIDTH_FT);
 const material = new THREE.MeshBasicMaterial({
 	vertexColors: false,
 });
@@ -34,24 +38,47 @@ const materials: THREE.MeshBasicMaterial[] = [
 	new THREE.MeshBasicMaterial({ color: 0xff00ff }),
 	new THREE.MeshBasicMaterial({ color: 0xffff00 }),
 	new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+	// new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
 ];
 
-const cube = new THREE.Mesh(geometry, materials);
+let table: Table = new Table(geometry, materials);
+
+// Table.createTable().then((ret_table) => {
+// 	table = ret_table;
+// });
+// const cube = new THREE.Mesh(geometry, materials);
+
+//
+// setup a ball
+//
+const geometry_ball = new THREE.SphereGeometry(BALL_DIM_FT, 32, 16);
+const material_ball = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const ball = new THREE.Mesh(geometry_ball, material_ball);
+scene.add(ball);
 
 // set up camera
-camera.position.set(0, 0, -20);
+camera.position.set(7, 7, -7);
 camera.lookAt(0, 0, 0);
 
 // add camera and cube to the scene
-scene.add(cube);
+scene.add(table);
 scene.add(camera);
 
 const clock = new THREE.Clock();
 
 // set initial position of cubes
-cube.position.x = 0;
-cube.position.y = 0;
-cube.position.z = 0;
+table.position.x = 0;
+table.position.y = -0.1;
+table.position.z = 0;
+
+ball.position.x = 0;
+ball.position.y = 0;
+ball.position.z = 0;
 
 //
 // initialize cube
