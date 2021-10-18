033ffebea8a15f950ac24dfbf0ee901e2682e55d
Dhrushit Raval <dr9703@rit.edu>
2021-10-17 22:07:51 -0400

    build files

--- Assignment2/dist/SceneManager.js
+++ Assignment2/dist/SceneManager.js
@@ -15,6 +15,7 @@ class SceneManager {
         ball.position.x = 0;
         ball.position.y = 0;
         ball.position.z = 0;
+        scene.add(new THREE.BoxHelper(ball));
     }
     createTable() {
         const materials = [
@@ -27,9 +28,45 @@ class SceneManager {
         ];
         return new Table(materials);
     }
+    detectBallsCollision() {
+        for (let i = 0; i < this.balls.length; i++) {
+            for (let j = i + 1; j < this.balls.length; j++) {
+                const ball1 = this.balls[i];
+                const ball2 = this.balls[j];
+                const ball1BB = this.balls[i].geometry.boundingBox;
+                const ball2BB = this.balls[j].geometry.boundingBox;
+                if (ball1BB.intersectsBox(ball2BB)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    detectBallCushionCollision() {
+        for (let ball of this.balls) {
+            ball.geometry.computeBoundingBox();
+            const ballBB = ball.geometry.boundingBox;
+            if (this.table.checkCollisionWithCushion(ballBB)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    detectCollision() {
+        return this.detectBallsCollision() || this.detectBallCushionCollision();
+    }
+    determineCollision() {
+        return [];
+    }
+    handleCollision(collidingObjects) { }
     myUpdate() {
         for (let ball of this.balls) {
             ball.myUpdate();
+        }
+        if (this.detectCollision()) {
+            console.log("Collision Detected");
+            const collidingOBjects = this.determineCollision();
+            this.handleCollision(collidingOBjects);
         }
     }
 }
--- Assignment2/dist/ball.js
+++ Assignment2/dist/ball.js
@@ -1,11 +1,12 @@
 class Ball extends THREE.Mesh {
     constructor(radius) {
-        const geometry_ball = new THREE.SphereGeometry(BALL_DIM_FT, 32, 32);
+        const geometry_ball = new THREE.SphereGeometry(Ball.BALL_DIM_FT, 32, 32);
         const material_ball = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         super(geometry_ball, material_ball);
         this.radius = radius;
     }
     myUpdate() { }
+    exertForce() { }
 }
-Ball.BALL_DIM_FT = 0.0859375;
+Ball.BALL_DIM_FT = 1;
 //# sourceMappingURL=Ball.js.map
