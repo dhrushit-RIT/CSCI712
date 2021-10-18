d8bc4bada2e9c7a2832bd7447261b5568e3e513e
Dhrushit Raval <dr9703@rit.edu>
2021-10-17 20:07:58 -0400

    updating the ball to know its radius

--- Assignment2/src/Ball.ts
+++ Assignment2/src/Ball.ts
@@ -1,20 +1,23 @@
 class Ball extends THREE.Mesh {
+	private radius: number;
+	private velocity: number;
+	private angvelocity: number;
+	private force: number;
+	private rotation_deg: number;
+	private frictional_coeff: number;
+	private momentum: number;
+	private angularmoment: number;
+	private mass: number;
+	private acceleration: number;
+	private angacceleration: number;
+	static BALL_DIM_FT = 0.0859375;
 
-    private radius:number;
-    private velocity: number;
-    private angvelocity: number;
-    private force: number;
-    private rotation_deg: number;
-    private frictional_coeff: number;
-    private momentum: number;
-    private angularmoment: number;
-    private mass: number;
-    private acceleration: number;
-    private angacceleration: number;
-
-	constructor(radius: number,geometry_ball: THREE.SphereGeometry,material_ball: THREE.MeshBasicMaterial) {
-		super(geometry_ball,material_ball);
-        this.radius=radius;
+	constructor(radius: number) {
+		const geometry_ball = new THREE.SphereGeometry(BALL_DIM_FT, 32, 32);
+		const material_ball = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+		super(geometry_ball, material_ball);
+		this.radius = radius;
 	}
 
+	myUpdate() {}
 }
