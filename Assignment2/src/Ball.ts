0e23a64f58eadbfc4577be7e30860a3f623890fe
Dhrushit Raval <dr9703@rit.edu>
2021-10-19 11:14:22 -0400

    update the types

--- Assignment2/src/Ball.ts
+++ Assignment2/src/Ball.ts
@@ -1,14 +1,14 @@
 class Ball extends THREE.Mesh {
 	private radius: number;
-	private velocity: number;
+	private velocity: THREE.Vector3;
 	private angvelocity: number;
-	private force: number;
+	private force: THREE.Vector3;
 	private rotation_deg: number;
 	private frictional_coeff: number;
 	private momentum: number;
 	private angularmoment: number;
 	private mass: number;
-	private acceleration: number;
+	private acceleration: THREE.Vector3;
 	private angacceleration: number;
 	// static BALL_DIM_FT = 1;
 	static BALL_DIM_FT = 0.0859375;
