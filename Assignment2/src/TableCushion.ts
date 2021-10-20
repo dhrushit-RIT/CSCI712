69c248ab8159c8b88d4ca04a80b0328cf1c983f1
Dhrushit Raval <dr9703@rit.edu>
2021-10-20 17:39:28 -0400

    adding surface normal for cushion

--- Assignment2/src/TableCushion.ts
+++ Assignment2/src/TableCushion.ts
@@ -1,11 +1,22 @@
 class TableCushion extends THREE.Mesh {
-    private length:number;
-    private frictionCoefficient = {
+	private length: number;
+	private surfaceNormal: THREE.Vector3;
+	private frictionCoefficient = {
 		ball: 0.3,
 	};
 
-    constructor(geometry: THREE.BoxGeometry, material: THREE.MeshBasicMaterial[], length: number){
-        super(geometry, material);
-        this.length = length;
-    }
+	constructor(
+		geometry: THREE.BoxGeometry,
+		material: THREE.MeshBasicMaterial[],
+		length: number,
+		surfaceNormal: THREE.Vector3
+	) {
+		super(geometry, material);
+		this.length = length;
+		this.surfaceNormal = surfaceNormal;
+	}
+
+	getSurfaceNormal(): THREE.Vector3 {
+		return this.surfaceNormal;
+	}
 }
