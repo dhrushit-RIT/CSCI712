72a6c82604fd7820ac136e86749da3e394686beb
Dhrushit Raval <dr9703@rit.edu>
2021-10-24 01:33:36 -0400

    build files

---
+++ Assignment2/dist/Table.js
@@ -0,0 +1,94 @@
+class Table extends THREE.Group {
+    constructor(material) {
+        super();
+        this.frictionCoefficient = {
+            ball: 0.3,
+        };
+        const surfaceGeometry = new THREE.BoxGeometry(Table.TABLE_LENGTH, 0.1, Table.TABLE_WIDTH);
+        const cushionGeometry = new THREE.BoxGeometry(Table.TABLE_WIDTH, 0.5, 0.1);
+        this.surface = new THREE.Mesh(surfaceGeometry, material);
+        this.cushion1 = new TableCushion("cushion1", cushionGeometry, material, Table.TABLE_WIDTH, new THREE.Vector3(-1, 0, 0));
+        this.cushion2 = new TableCushion("cushion2", cushionGeometry, material, Table.TABLE_LENGTH, new THREE.Vector3(0, 0, -1));
+        this.cushion3 = new TableCushion("cushion3", cushionGeometry, material, Table.TABLE_WIDTH, new THREE.Vector3(0, 0, -1));
+        this.cushion4 = new TableCushion("cushion4", cushionGeometry, material, Table.TABLE_LENGTH, new THREE.Vector3(1, 0, 0));
+        this.cushion5 = new TableCushion("cushion5", cushionGeometry, material, Table.TABLE_LENGTH, new THREE.Vector3(0, 0, 1));
+        this.cushion6 = new TableCushion("cushion6", cushionGeometry, material, Table.TABLE_LENGTH, new THREE.Vector3(0, 0, 1));
+        this.cushion1.rotateY(Math.PI / 2);
+        this.cushion4.rotateY(Math.PI / 2);
+        this.cushion1.position.setX(Table.TABLE_WIDTH);
+        this.cushion4.position.setX(-Table.TABLE_WIDTH);
+        this.cushion2.position.setX(Table.TABLE_WIDTH / 2);
+        this.cushion2.position.setZ(Table.TABLE_WIDTH / 2);
+        this.cushion3.position.setX(-Table.TABLE_WIDTH / 2);
+        this.cushion3.position.setZ(Table.TABLE_WIDTH / 2);
+        this.cushion5.position.setX(-Table.TABLE_WIDTH / 2);
+        this.cushion5.position.setZ(-Table.TABLE_WIDTH / 2);
+        this.cushion6.position.setX(Table.TABLE_WIDTH / 2);
+        this.cushion6.position.setZ(-Table.TABLE_WIDTH / 2);
+        this.cushion1.geometry.computeBoundingBox();
+        this.cushion2.geometry.computeBoundingBox();
+        this.cushion3.geometry.computeBoundingBox();
+        this.cushion4.geometry.computeBoundingBox();
+        this.cushion5.geometry.computeBoundingBox();
+        this.cushion6.geometry.computeBoundingBox();
+        this.add(this.surface);
+        this.add(this.cushion1);
+        this.add(this.cushion2);
+        this.add(this.cushion3);
+        this.add(this.cushion4);
+        this.add(this.cushion5);
+        this.add(this.cushion6);
+    }
+    checkCollisionWithCushion(ball) {
+        const ballBB = ball.geometry.boundingBox.clone();
+        ballBB.applyMatrix4(ball.matrixWorld);
+        let cushion1BB = this.cushion1.geometry.boundingBox.clone();
+        let cushion2BB = this.cushion2.geometry.boundingBox.clone();
+        let cushion3BB = this.cushion3.geometry.boundingBox.clone();
+        let cushion4BB = this.cushion4.geometry.boundingBox.clone();
+        let cushion5BB = this.cushion5.geometry.boundingBox.clone();
+        let cushion6BB = this.cushion6.geometry.boundingBox.clone();
+        cushion1BB.applyMatrix4(this.cushion1.matrixWorld);
+        cushion2BB.applyMatrix4(this.cushion2.matrixWorld);
+        cushion3BB.applyMatrix4(this.cushion3.matrixWorld);
+        cushion4BB.applyMatrix4(this.cushion4.matrixWorld);
+        cushion5BB.applyMatrix4(this.cushion5.matrixWorld);
+        cushion6BB.applyMatrix4(this.cushion6.matrixWorld);
+        const ballVel = ball.getVelocity().clone();
+        const ballMass = ball.getMass();
+        const impulse = ballVel
+            .multiplyScalar(-1 * (1 + SceneManager.ELASTICITY_BALL_CUSHION))
+            .multiplyScalar(ballMass);
+        if (cushion1BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion1, ball, impulse);
+        }
+        if (cushion2BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion2, ball, impulse);
+        }
+        if (cushion3BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion3, ball, impulse);
+        }
+        if (cushion4BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion4, ball, impulse);
+        }
+        if (cushion5BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion5, ball, impulse);
+        }
+        if (cushion6BB.intersectsBox(ballBB)) {
+            this.handleCollision(this.cushion6, ball, impulse);
+        }
+    }
+    handleCollision(cushion, ball, impulse) {
+        SceneManager.collisionTracker[ball.getBallName()].add(cushion);
+        const magImpulseOnNormal = Math.abs(impulse.dot(this.cushion1.getSurfaceNormal()));
+        const impulseOnBall = cushion
+            .getSurfaceNormal()
+            .clone()
+            .setLength(magImpulseOnNormal);
+        ball.goBack();
+        ball.applyImpulse(impulseOnBall);
+    }
+}
+Table.TABLE_WIDTH = 6;
+Table.TABLE_LENGTH = 12;
+//# sourceMappingURL=Table.js.map
---
+++ Assignment2/dist/app.js
@@ -0,0 +1,25 @@
+const scene = new THREE.Scene();
+const cnv = document.getElementById("c");
+const side = window.innerWidth;
+const renderer = new THREE.WebGLRenderer({ canvas: cnv });
+let fieldOfView = 45, aspectRatio = 4 / 3, near = 0.1, far = 1000;
+const camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
+document.body.appendChild(renderer.domElement);
+const material = new THREE.MeshBasicMaterial({
+    vertexColors: false,
+});
+const sceneManager = new SceneManager(scene);
+camera.position.set(7, 7, 0);
+camera.lookAt(0, 0, 0);
+scene.add(camera);
+const clock = new THREE.Clock();
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+let endTimeFactor = 1;
+function animate() {
+    let handle = requestAnimationFrame(animate);
+    sceneManager.myUpdate(clock.getElapsedTime());
+    renderer.render(scene, camera);
+}
+animate();
+//# sourceMappingURL=app.js.map
